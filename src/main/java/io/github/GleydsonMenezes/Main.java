32e43356a3a4990117be4bfdec8ef9a361e86a0b
Gleydson Menezes <gsmenezes996@gmail.com>
2024-08-13 16:01:34 -0300

    RestController V2

--- src/main/java/io/github/GleydsonMenezes/Main.java
+++ src/main/java/io/github/GleydsonMenezes/Main.java
@@ -1,13 +1,8 @@
 package io.github.GleydsonMenezes;
 
 
-import io.github.GleydsonMenezes.domain.entity.Cliente;
-import io.github.GleydsonMenezes.domain.repository.Clientes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
